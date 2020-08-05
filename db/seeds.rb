cacd9ceb470c41b178cfa77b50be133b006f8175
Kristian Portillo <kristianportillo@Kristians-MacBook-Air.local>
2020-08-04 22:20:06 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "Help me God", description:"I really need to how to make webpages")
